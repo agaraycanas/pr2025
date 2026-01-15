edcbf3fbed1963d860962deb2b43b3cc142ecf13
ALBERTO_IES <agaraycanas@educa.madrid.org>
2026-01-15 14:21:26 +0100

    casi terminado 15 (sin polimorfismo)

--- src/t5/b/ej15/MainConCuadradosYCirculos.java
+++ src/t5/b/ej15/MainConCuadradosYCirculos.java
@@ -5,10 +5,19 @@ public class MainConCuadradosYCirculos { // EJERCICIO f
 	public static void main(String[] args) {
 		System.out.println("CON CUADRADOS Y CIRCULOS MEZCLADOS");
 
-		Cuadrado[] cuadrado = new Cuadrado[args.length/2];
+		Cuadrado[] 	cuadrado = new Cuadrado[args.length/2];
+		Circulo[] 	circulo = new Circulo[args.length/2];
+		int sigCuadrado = 0;
+		int sigCirculo = 0;
+		
 		for (int i=0;i<args.length;i+=2) {
 			if (args[i].equals("cu")) {
-				cuadrado[i/2] = new Cuadrado(Float.parseFloat(args[i+1]));
+				cuadrado[ sigCuadrado ] = new Cuadrado(Float.parseFloat(args[i+1]));
+				sigCuadrado++;
+			}
+			if (args[i].equals("ci")) {
+				circulo[ sigCirculo ] = new Circulo(Float.parseFloat(args[i+1]));
+				sigCirculo++;
 			}
 		}
 		
