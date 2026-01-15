edb5d65f66d4c491d91c91903c398f64f26b4e0b
ALBERTO_IES <agaraycanas@educa.madrid.org>
2026-01-15 14:15:22 +0100

    ejercicio 15 sigue

--- src/t5/b/ej15/MainConCuadradosYCirculos.java
+++ src/t5/b/ej15/MainConCuadradosYCirculos.java
@@ -5,12 +5,16 @@ public class MainConCuadradosYCirculos { // EJERCICIO f
 	public static void main(String[] args) {
 		System.out.println("CON CUADRADOS Y CIRCULOS MEZCLADOS");
 
-		Cuadrado[] cuadrado = new Cuadrado[args.length];
-		for (int i=0;i<cuadrado.length;i++) {
-			cuadrado[i] = new Cuadrado(Float.parseFloat(args[i]));
+		Cuadrado[] cuadrado = new Cuadrado[args.length/2];
+		for (int i=0;i<args.length;i+=2) {
+			if (args[i].equals("cu")) {
+				cuadrado[i/2] = new Cuadrado(Float.parseFloat(args[i+1]));
+			}
 		}
+		
 		float areaTotal = 0;
 		float precioTotal = 0;
+		
 		for (int i=0;i<cuadrado.length;i++) {
 			areaTotal 	+=  cuadrado[i].area();
 			precioTotal +=  cuadrado[i].precio();
