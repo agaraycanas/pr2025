c1d83ca8e6f4623281774ca483c7776afb956b6e
ALBERTO_IES <agaraycanas@educa.madrid.org>
2026-02-25 12:28:41 +0100

    Pendiente de terminar ejercicio 6 de ficheros

--- src/t10/ej06/Main.java
+++ src/t10/ej06/Main.java
@@ -1,12 +1,54 @@
 package t10.ej06;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class Main {
-	public static void main(String[] args) {
-		if (args.length != 1) {
-			System.out.println("Debes introducir UN argumento de entrada");
-			System.out.println("Uso: t10.ej06.Main [numeroDeArchivos]");
-		} else {
-			String carpeta = "src/t10/ej06/";
+	private static String rutaAlEjercicio = "src/t10/ej06/";
+
+	private static void comprobarArgumentosDeEntrada(String[] args, int numero, String uso) {
+		if (args.length != numero) {
+			System.out.println("ERROR: Número de argumentos de entrada incorrectos");
+			System.out.println("Uso: " + uso);
+			throw new IllegalArgumentException();
 		}
+	}
+
+	public static void main(String[] args) {
+		try {
+			comprobarArgumentosDeEntrada(args, 1, "t10.ej06.Main [numeroDeArchivos]");
+			crearCarpetas();
+			crearArchivos(Integer.parseInt(args[0]));
+			System.out.println("FIN");
+		} catch (IllegalArgumentException e) {
+		} catch (IOException e) {
+			System.out.println("ERROR de ENTRADA/SALIDA");
+			e.printStackTrace();
+		} catch (Exception e) {
+			System.out.println("ERROR GENÉRICO");
+			e.printStackTrace();
+		}
+	}
+
+	private static void crearArchivos(int numeroArchivos) throws Exception {
+		String carpeta = "/uno/";
+		for (int i=1;i<=numeroArchivos;i++) {
+			Path ruta = Path.of( rutaAlEjercicio + carpeta + "f" + i + ".txt");
+			Files.writeString(ruta, "TEST");
+		}
+	}
+
+	private static void crearCarpetas() throws IOException {
+		if (!Files.exists(Path.of(rutaAlEjercicio + "uno"))) {
+			Files.createDirectory(Path.of(rutaAlEjercicio + "uno"));
+		}
+		if (!Files.exists(Path.of(rutaAlEjercicio + "dos"))) {
+			Files.createDirectory(Path.of(rutaAlEjercicio + "dos"));
+		}
+		if (!Files.exists(Path.of(rutaAlEjercicio + "tres"))) {
+			Files.createDirectory(Path.of(rutaAlEjercicio + "tres"));
+		}
+
 	}
 }
